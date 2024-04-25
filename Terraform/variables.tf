27d8757980224d3978569a0c12946e969f2326bb
Julian Galeano Rojas <Julian.Galeano.Rojas@ey.com>
2024-04-24 20:34:48 -0500

    Completed PArt 4

--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -13,3 +13,31 @@ variable "aws-region" {
 variable "ami-id" {
     type = string
 }
+
+variable "admin-username" {
+  type = string
+}
+
+variable "admin-password" {
+  type = string
+}
+
+variable "admin-fullname" {
+  type = string
+}
+
+variable "admin-email" {
+  type = string
+}
+
+variable "remote-repo" {
+  type = string
+}
+
+variable "job-name" {
+  type = string
+}
+
+variable "secrets" {
+  type = map(string)
+}
